5754bce11f6b4f114eb4e70b3d9c101322b4ab1d
Sara Dastpak <96159156+Sara-Dastpak@users.noreply.github.com>
2022-10-25 21:20:34 +0330

    Update EX1_2.py

--- EX1/EX1_2.py
+++ EX1/EX1_2.py
@@ -1,4 +1,10 @@
 def EX1_2(base, height):
+  '''
+  (float, float) -> float
+  Return the area of the triangle
+  >>> EX1_2(4, 3)
+  6
+  '''
   if base > 0 and height > 0:
     area = (base * height)/2
     return area
