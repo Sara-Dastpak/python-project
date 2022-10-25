81daa88a7c13120a14d1199bb4a04d875f28a0a6
Sara Dastpak <96159156+Sara-Dastpak@users.noreply.github.com>
2022-10-25 21:30:40 +0330

    Update EX1_6.py

--- EX1/EX1_6.py
+++ EX1/EX1_6.py
@@ -1,4 +1,10 @@
 def EX1_6(num):
+  '''
+  (int) -> int
+  convert num to roman numbers
+  >>> EX1_6(5)
+  V
+  '''
   romanversion = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
   if type(num) == int:
     if num > 0 and num <= 10:
