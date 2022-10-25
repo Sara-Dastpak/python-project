0c1de239f89a82d77bdd7f3b8cf4ec484176abb0
Sara Dastpak <96159156+Sara-Dastpak@users.noreply.github.com>
2022-10-25 21:24:58 +0330

    Update EX1_3.py

--- EX1/EX1_3.py
+++ EX1/EX1_3.py
@@ -1,4 +1,10 @@
 def EX1_3(side1, side2):
+  '''
+  (int, int) -> int
+  return the range of the number that the side 3 would be between them
+  >>> EX1_3(5, 4)
+  8
+  '''
   if type(side1) == int and type(side2) == int:
     if side1 > 0 and side2 > 0:
       range = side1 + side2 - 1
