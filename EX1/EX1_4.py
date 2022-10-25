6590e55c3bfbbcb66e76bc5c05477b500501946a
Sara Dastpak <96159156+Sara-Dastpak@users.noreply.github.com>
2022-10-25 21:26:42 +0330

    Update EX1_4.py

--- EX1/EX1_4.py
+++ EX1/EX1_4.py
@@ -1,4 +1,10 @@
 def EX1_4(num):
+  '''
+  (int) -> int
+  return the day that num was asign to
+  >>>EX1_4(5)
+  Friday
+  '''
   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   if type(num) == int:
     if num > 0 and num < 8:
